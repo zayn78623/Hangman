af3126037ea0fd8a1c27bec78071f8fe3054ccc4
zayn <zayn@DESKTOP-N203LU0>
2019-03-30 20:41:25 +0500

    Factorial

--- hangman.c
+++ hangman.c
@@ -3,11 +3,28 @@
 #include<stdlib.h>
 #include<string.h>
 
-void hangmanword(int);
+int fact(int);
 int main()
 {
   char hangmanword[100];
+  int num;
   printf("This has been hell, it better be working now or else im done with this shit.");
   _getch(); 
-  so youre saying k c=visual se push  ni hoskta?
+  so youre saying k c = visual se push  ni hoskta;
+  printf("Enter number:");
+  scanf_s("%d", &num);
+  printf("%d! = %d ", num, fact(num));
+  _getch();
+}
+int fact(int n)
+{
+	n = 1;
+	int loop;
+	int result;
+
+	for ( loop = 1; loop <= n ; loop++)
+	{
+		result = fact*loop;
+		return result;
+	}
 }
