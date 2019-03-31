7baebc97a78ac7d3b9458cd7b8c432f198a8ab5f
Ubaid <Ubaid@DESKTOP-B7N9SGB>
2019-03-31 13:28:58 +0500

    Tried this

--- Project1/Project1/Source.c
+++ Project1/Project1/Source.c
@@ -2,18 +2,38 @@
 #include<conio.h>
 #include<ctype.h>
 
-
 void main() {
+	int i, j, k;
+	char Words[][3] = { "Hey", "How", "Are", "You" };
+	char word;
 
-	printf("*******************************************************************************\n");
-	printf("**   **\n");
-	printf("**   **\n");
-	printf("**   **\n");
-	printf("*******\n");
-	printf("**   **\n");
-	printf("**   **\n");
-	printf("**   **\n");
-	printf("*******************************************************************************\n");
+	for (i = 0; i < 4; i++) {
+		printf("enter a word:");
+		word = _getche();
 
+		if (Words[i][0] == word) {
+			printf("\nGood Job");
+			_getch();
+		}
+		else if (Words[i][1] == word)
+		{
+			printf("\nGood Job");
+				_getch();
+		}
+		else if (Words[i][2] == word)
+		{
+			printf("\nGood Job");
+			_getch();
+		}
+		else if (Words[i][3] == word)
+		{
+			printf("\nGood Job");
+			_getch();
+		}else{
+			printf("\nTry Again");
+		}
+		system("cls");
+	}
 	system("pause");
+
 }
