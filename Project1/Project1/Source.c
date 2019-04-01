9495850c7c88a5124f72d6110b96b7d028bd1b2e
Abaddon <38532188+UbaidSarhandi@users.noreply.github.com>
2019-04-01 14:03:46 +0500

    Update Source.c

--- Project1/Project1/Source.c
+++ Project1/Project1/Source.c
@@ -16,9 +16,12 @@ void main() {
 	int numoflifes = 5;
 	int numcorrect = 0;
 	int oldcorrect = 0;
-
-
+	char letterentered;
+	int letterguessed[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int quit = 0;
 	char ch;
+oxy:
+
 	printf("\t\t--------------------------------------------------------------------------\n");
 	printf("\t\t| #      #      ##      #     #   #######   #      #      ##     #     # |\n");
 	printf("\t\t| #      #     #  #     ##    #   #         ##     #     #  #    ##    # |\n");
@@ -33,108 +36,103 @@ void main() {
 	printf("\n\n\n\n\n\t\t\t\tPress P to play\n\t\t\t\tPress I for Instructions\n\t\t\t\tPresss C for Credits\n\t\t\t\tPress Q to Quit\n\n\t\t\t\tYour Choice: ");
 	scanf_s("%c", &ch);
 
-	//						  0 1 2 3 4 5
-	//						  W A T E R M E L L O N
-	int letterguessed[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-
-	int quit = 0;
-	char letterentered;
-
-	switch (ch)
+	
+	if (ch == 'P' || ch == 'p' || ch == 'C' || ch == 'c' || ch == 'I' || ch == 'i' || ch == 'Q' || ch == 'q')
 	{
-	case 'P':
-	case'p':
-
-
-		system("cls");
-		Sleep(500);
-		printf("loading.");
-		Sleep(500);
-		printf(".");
-		Sleep(500);
-		printf(".");
-		Sleep(500);
-		printf(".");
-		Sleep(500);
-		printf(".");
-		system("cls");
-
-		printf("\nGuess Words : %s randomindex:%d lengthofwords: %d \n",
-			Words[randomindex], randomindex, lengthofword);
-
-		//game loop
-		while (numcorrect < lengthofword)
+		switch (ch)
 		{
-			printf("Next Turn \n WORD : ");
-			for (i = 0; i < lengthofword; i++)
+		case 'P':
+		case'p':
+
+
+			system("cls");
+			Sleep(500);
+			printf("loading.");
+			Sleep(500);
+			printf(".");
+			Sleep(500);
+			printf(".");
+			Sleep(500);
+			printf(".");
+			Sleep(500);
+			printf(".");
+			system("cls");
+
+			printf("\nGuess Words : %s randomindex:%d lengthofwords: %d \n",
+				Words[randomindex], randomindex, lengthofword);
+
+			//game loop
+			while (numcorrect < lengthofword)
 			{
-
-				if (letterguessed[i] == 1) {
-					printf("%c", Words[randomindex][i]);
-				}
-				else
+				printf("Next Turn \n WORD : ");
+				for (i = 0; i < lengthofword; i++)
 				{
-					printf("-");
-				}
-			}
 
-			printf("\nscore:%d\n", numcorrect);
-			printf("Lives Left :%d\n", numoflifes);
-			printf("enter a letter:");
-			fgets(guess, 16, stdin);
-
-			if (strncmp(guess, "quit", 4) == 0) {
-				quit = 1;
-				break;
-			}
-
-			letterentered = guess[0];
-			printf("letter Entered:%c \n", letterentered);
-
-			oldcorrect = numcorrect;
-
-			for (i = 0; i < lengthofword; i++)
-			{
-				if (letterguessed[i] == 1)
-				{
-					continue;
-				}
-				if (letterentered == Words[randomindex][i])
-				{
-					letterguessed[i] = 1;
-					++numcorrect;
+					if (letterguessed[i] == 1) {
+						printf("%c", Words[randomindex][i]);
+					}
+					else
+					{
+						printf("-");
+					}
 				}
 
+				printf("\nscore:%d\n", numcorrect);
+				printf("Lives Left :%d\n", numoflifes);
+				printf("enter a letter:");
+				fgets(guess, 16, stdin);
 
-
-
-			}
-			if (oldcorrect == numcorrect)
-			{
-				numoflifes--;
-				printf("sorry Wrong guess\n");
-
-				if (numoflifes == 0) {
+				if (strncmp(guess, "quit", 4) == 0) {
+					quit = 1;
 					break;
 				}
 
-			}
-			else
-			{
-				printf("Correct guess\n");
-			}
+				letterentered = guess[0];
+				printf("letter Entered:%c \n", letterentered);
+
+				oldcorrect = numcorrect;
+
+				for (i = 0; i < lengthofword; i++)
+				{
+					if (letterguessed[i] == 1)
+					{
+						continue;
+					}
+					if (letterentered == Words[randomindex][i])
+					{
+						letterguessed[i] = 1;
+						++numcorrect;
+					}
 
 
-			MessageBeep(400);
-			Sleep(1000);
-			system("cls");
+
+
+				}
+				if (oldcorrect == numcorrect)
+				{
+					numoflifes--;
+					printf("sorry Wrong guess\n");
+
+					if (numoflifes == 0) {
+						break;
+					}
+
+				}
+				else
+				{
+					printf("Correct guess\n");
+				}
+
+
+				MessageBeep(400);
+				Sleep(1000);
+				system("cls");
 
 
 
-	
-		}//while loop end
-		break;
+
+			}//while loop end
+			break;
 
 		case 'C':
 		case'c':
@@ -167,22 +165,29 @@ void main() {
 		case'q':
 			quit = 1;
 			break;
+		}
+
+		system("cls");
+
+		if (quit == 1)
+		{
+			printf("\nYOU QUIT!!\n");
+		}
+		else if (numoflifes == 0)
+		{
+			printf("\n  you lost.\n");
+		}
+		else {
+			printf("YOU WIN !!\n");
+		}
 	}
-
-
+else
+{
+	printf("\n\t\t\t\t Wrong CHOICE!!\n\t\t\t\t Enter Again");
 	system("cls");
+	goto oxy;
+}
 
-	if (quit == 1)
-	{
-		printf("\nYOU QUIT!!\n");
-	}
-	else if (numoflifes == 0)
-	{
-		printf("\n  you lost.\n");
-	}
-	else {
-		printf("YOU WIN !!\n");
-	}
 	_getch();
 
 
